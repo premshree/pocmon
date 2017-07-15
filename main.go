eaec49ad2fa0c44a5e7a190c7f320cfade222f17
Premshree Pillai <premshree@Premshrees-MBP.home>
2017-07-15 12:03:23 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "regexp"
+  "time"
+
+	"github.com/nlopes/slack"
+  "github.com/robfig/cron"
+  "github.com/spf13/viper"
+
+	"log"
+)
+
+type Config struct {
+  RotateFrequency string `mapstructure:"rotate_frequency"`
+  PocMessagePattern string `mapstructure:"poc_message_patten"`
+  MessagePocChange string `mapstructure:"message_poc_change"`
+  ExcludedRotators map[string]bool `mapstructure:"excluded_rotators"`
+}
+
+var (
+  token string // set this in env
+	api *slack.Client
+	config Config
+  rotators map[string][]string // rotators by channel
+  rotated map[string]map[string]bool
+)
+
+func init() {
+  viper := viper.New()
+  viper.SetConfigFile("./config.json")
+  viper.AutomaticEnv()
+	viper.ReadInConfig()
+
+  log.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+  token = viper.GetString("POCMON_TOKEN")
+  c := viper.Sub("config")
+  err := c.Unmarshal(&config)
+  if err != nil {
+    log.Fatalf("unable to decode config into struct, %v", err)
+  }
+}
+
+func main() {
+	api = slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+  rand.Seed(time.Now().Unix())
+  rotators = make(map[string][]string)
+  rotated = make(map[string]map[string]bool)
+
+  cron := cron.New()
+  cron.AddFunc(config.RotateFrequency, rotate)
+  cron.Start()
+
+  select { }
+}
+
+func rotate() {
+  channels, _ := api.GetChannels(false)
+  for _, channel := range channels {
+    if !channel.IsMember {
+      continue
+    }
+
+    rotators[channel.Name] = getAvailableRotators(channel, false)
+    rotators[channel.Name] = shuffleSlice(rotators[channel.Name])
+
+    rotator := rotators[channel.Name][0]
+    if rotated[channel.Name] == nil {
+      rotated[channel.Name] = make(map[string]bool)
+    }
+    rotated[channel.Name][rotator] = true
+
+    updateChannelTopic(channel, rotator)
+    sendMessageToRotator(channel, rotator)
+  }
+}
+
+func getAvailableRotators(channel slack.Channel, replenish bool) []string {
+  var rotators []string
+  for _, member := range channel.Members {
+    user, err := api.GetUserInfo(member)
+    if err != nil {
+      log.Fatal("Error getting channel members")
+    }
+    if config.ExcludedRotators[user.Name] {
+      continue
+    }
+    if !replenish && rotated[channel.Name][user.Name] {
+      continue
+    }
+
+    rotators = append(rotators, user.Name)
+  }
+
+  if rotators == nil {
+    rotated = make(map[string]map[string]bool) // reset rotated
+    rotators = getAvailableRotators(channel, true)
+  }
+
+  fmt.Printf("Rotators: %v\n", rotators)
+  return rotators
+}
+
+func sendMessageToRotator(channel slack.Channel, rotator string) {
+  _, _, errPostMessage := api.PostMessage(
+    channel.Name,
+    fmt.Sprintf(config.MessagePocChange, rotator, channel.Name),
+    slack.PostMessageParameters{})
+  if errPostMessage != nil {
+    log.Fatal(errPostMessage)
+  }
+}
+
+func updateChannelTopic(channel slack.Channel, rotator string) {
+  var topic string
+
+  r := regexp.MustCompile("%s")
+  pocPattern := r.ReplaceAllString(config.PocMessagePattern, "[a-z]+")
+  r = regexp.MustCompile(pocPattern)
+  if r.MatchString(channel.Topic.Value) {
+    topic = r.ReplaceAllString(channel.Topic.Value, fmt.Sprintf(config.PocMessagePattern, rotator))
+  } else if channel.Topic.Value != "" {
+    topic = fmt.Sprintf(channel.Topic.Value + " | " + config.PocMessagePattern, rotator)
+  } else {
+    topic = fmt.Sprintf(config.PocMessagePattern, rotator)
+  }
+
+  _, err := api.SetChannelTopic(channel.ID, topic)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+func shuffleSlice(slice []string) []string {
+  for i := range slice {
+    j := rand.Intn(i + 1)
+    slice[i], slice[j] = slice[j], slice[i]
+  }
+  return slice
+}
